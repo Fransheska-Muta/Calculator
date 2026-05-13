8aed9330449266ee3f71f463088f38c1e0c3a3af
mutafransheska45-maker <mutafransheska45@gmail.com>
2026-05-13 20:10:21 +0200

    Left with 2 operators

--- index.js
+++ index.js
@@ -1,15 +1,66 @@
-//this says that whatever value is clicked on, display it on the input
-const display = document.getElementById("display");
-function appendToDisplay(input){
-    display.value += input;
+// 1. press one number and save it
+// 2.press the operator and save it
+// 3.press second number and save it
+// 4.press equal sign and make calculation
+
+let firstNumber
+let secondNumber
+let step = 0
+let operation
+let result = 0
+
+let numArray = []
+let secondNumArray = []
+
+let display = document.getElementById('display')
+
+function getNumber(num) {
+    if(step === 0 || step === 1){
+        numArray.push(num)
+        step = 1
+        firstNumber = Number(numArray.join(''))
+        display.value = firstNumber
+    }else if(step === 2){
+      secondNumArray.push(num)
+      secondNumber = Number(secondNumArray.join(''))
+      display.value = secondNumber
+    }
+}
+function getOperator(op) {
+   step = 2
+   operation = op
+   display.value = operation
 }
 
-//this clears the values once AC is clicked
-function clearDisplay(){
-    display.value = "";
+function clearDisplay() {
+    display.value = 0
+    firstNumber = null
+    secondNumber = null
+    step = 0
+    operation = null
+    result = 0
+    numArray = []
+    secondNumArray = []
 }
 
-//this is the function that calculates teh values
-function calculate(){
-    display.value = eval(display.value)
+const calculateEquals = () => {
+    if(operation === '+'){
+       result = firstNumber + secondNumber
+       display.value = result
+    }else if (operation === '-'){
+        result = firstNumber - secondNumber
+        display.value = result
+    }else if (operation === '*'){
+        result = firstNumber * secondNumber
+        display.value = result
+    }else if(operation === '/'){
+        result = firstNumber / secondNumber
+        display.value = result
+    }else if (operation === '%'){
+        result = firstNumber % secondNumber
+        display.value = result
+    }else if (operation === '√'){
+        result = Math.sqrt(firstNumber)
+        display.value = result
+    }
 }
